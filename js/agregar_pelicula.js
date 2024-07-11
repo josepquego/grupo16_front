28a01e6969cf7c925c774b632da0342855840dd4
¨julievera7¨ <julie@julie.168.1.25>
2024-07-11 16:55:20 -0300

    Tabla - datos de bd - No se ha probado

--- js/agregar_pelicula.js
+++ js/agregar_pelicula.js
@@ -1,43 +1,46 @@
-
 // validacion de campos de formulario cuando se carga el dom
-document.addEventListener('DOMContentLoaded', function() {
-
-    const formPelicula = document.getElementById('formPelicula');
-    formPelicula.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const titulo = document.getElementById('titulo').value;
-        const genero = document.getElementById('genero').value;
-        const duracion = document.getElementById('duracion').value;
-        const imagen = document.getElementById('imagen').value;
-        // verificar si los campos estan vacios
-        // si alguno de los campos esta vacio se muestra un mensaje de error en el div correspondiente al input
-        // levanto los campos de error
-        const errorTitulo = document.getElementById('error-titulo');
-        const errorGenero = document.getElementById('error-genero');
-        const errorDuracion = document.getElementById('error-duracion');
-        console.log("entro a la funcion");
-        // limpio los mensajes de error
-        errorTitulo.innerText = "";
-        errorGenero.innerText = "";
-        errorDuracion.innerText = "";
-
-
-        if (titulo.value === '' || titulo.value == null) {
-            e.preventDefault();
-            errorTitulo.innerText = "El título de la película es obligatorio";
+const validarPelicula = () => {
+    document.getElementById("formPelicula").addEventListener("submit", function(event) {
+        event.preventDefault();
+        resetErrorMessages();
+        //Validar campos
+        let titulo = document.getElementById('titulo').value.trim();
+        let genero = document.getElementById('genero').value.trim();
+        let duracion = document.getElementById('duracion').value.trim();
+        let imagen = document.getElementById('imagen').value.trim();
+        let isValid = True;
+        //Mensajes
+        if (titulo === '' || titulo.value == null)  {
+            displayErrorMessages ("titulo-error", "* Campo obligatorio");
+            isValid = false;
         }
-        if (genero.value === '' || genero.value == null) {
-            e.preventDefault();
-            errorGenero.innerText = "El género es obligatorio";
+        if (genero === '') {
+            displayErrorMessages ("genero-error", "* Campo obligatorio");
+            isValid = false;
         }
-        if (duracion.value === '' || duracion.value == null) {
-            e.preventDefault();
-            errorDuracion.innerText = "La duración de la película es obligatoria";
+        if (duracion === '') {
+            displayErrorMessages ("duracion-error", "* Campo obligatorio");
+            isValid = false;
         }
-
-      
-        // objeto literal de pelicula 
-        // pegar a la api POST /peliculas
+        if (isValid) {
+            alert("Pelicula agregada correctamente");
+        }
 
     });
-});
+}
+
+
+function displayErrorMessages(elementId, message){
+    let errorElement = document.getElementById(elementId);
+    errorElement.innerText = message;
+}
+
+function resetErrorMessages() {
+    let errorElements = document.querySelectorAll(".error");
+    errorElements.forEach(function(element){
+        element.innerText = "";
+    });
+}
+
+document.addEventListener('DOMContentLoaded', validarPelicula);
+
